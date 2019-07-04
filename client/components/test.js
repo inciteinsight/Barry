3071dd91216ef5bc76017de3943c0edc51cb88f1
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-04 17:25:32 -0400

    Completed MVP of Test Component

--- client/components/test.js
+++ client/components/test.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {midyearproc2019} from '../store/lyricTempStore'
 
 class Test extends Component {
   constructor() {
@@ -27,112 +28,27 @@ class Test extends Component {
           enable: true
         }
       },
-      piece: {
-        name: 'Mid-Year Processional 2019',
-        sequence: [
-          'stanza1',
-          'stanza2',
-          'refrain',
-          'stanza3',
-          'stanza4',
-          'refrain',
-          'stanza5',
-          'stanza6',
-          'refrain',
-          'coda'
-        ],
-        parts: [
-          {
-            name: 'stanza1',
-            lines: [
-              'Great indeed is Your unfailing love',
-              'For us, O Lord God',
-              'You have guided us and we remain',
-              'Your true chosen ones',
-              'And this is a special day',
-              'In your blessed and dear Church of Christ'
-            ]
-          },
-          {
-            name: 'stanza2',
-            lines: [
-              'Oh we celebrate and bring to You',
-              'Thanksgiving and praise',
-              'For the blessings and great victories',
-              'From your boundless grace',
-              'And with You, we overcome',
-              'Every hindrance and trial we face'
-            ]
-          },
-          {
-            name: 'refrain',
-            lines: [
-              'Oh, our precious and great election',
-              'This, we will never waste, Lord God',
-              'A gift received by those redeemed',
-              'Saved through the sacrifice of Christ',
-              'We will not separate from the right',
-              'To worship You, which You bestowed on us'
-            ]
-          },
-          {
-            name: 'stanza3',
-            lines: [
-              'Through fierce persecution, You are there',
-              'Defending Your fold',
-              'In the darkest hours of our distress',
-              'You rescue Your own',
-              'O Lord God, You are so good',
-              'To your Church, where Your grace is bestowed'
-            ]
-          },
-          {
-            name: 'stanza4',
-            lines: [
-              'Your last mission of salvation grows',
-              'Now spanning the globe',
-              'Many join to praise and worship you',
-              'In reverence Lord',
-              'The true chosen who will reap',
-              'The reward that You have laid instore'
-            ]
-          },
-          {
-            name: 'stanza5',
-            lines: [
-              "All our efforts, we'll intensify",
-              'To help spread the truth',
-              'In the work of propagation, Lord',
-              'To always bear fruit',
-              'So that you are greatly pleased',
-              'We will spend all our days serving You'
-            ]
-          },
-          {
-            name: 'stanza6',
-            lines: [
-              'Lord, to Your whole nation, You give heed',
-              'And answer our cries',
-              'That the church administration, Lord',
-              "You'll care for and guide",
-              'So that we will all be led',
-              'To achieve perfect faith in Your sight'
-            ]
-          },
-          {
-            name: 'coda',
-            lines: [
-              'We will not separate from the right',
-              'To worship You, which You bestowed on us',
-              'Amen'
-            ]
-          }
-        ]
-      }
+      piece: midyearproc2019
     }
   }
 
   componentDidMount() {}
+
+  renderPart = (partName, filter = false) => {
+    const part = this.state.piece.parts.find(
+      partInstance => partInstance.name === partName
+    )
+    return (
+      <div>
+        <p className="text-center">
+          <strong>{part.name}</strong>
+        </p>
+        {part.lines.map(line => (
+          <p>{filter ? line.replace(/[^\w\s]+/g, '').toLowerCase() : line}</p>
+        ))}
+      </div>
+    )
+  }
 
   renderPiece() {
     // let {piece} = this.props
@@ -141,28 +57,15 @@ class Test extends Component {
     return (
       <div>
         <h4 className="text-center">{piece.name}</h4>
-        <div className="container d-flex justify-content-center">
-          <div className="form-container">
-            {piece.parts.map(part => (
-              <div>
-                <p>
-                  <strong>{part.name}</strong>
-                </p>
-                {part.lines.map(line => <p>{line}</p>)}
-                {/* {part.lines.map(line => <p>{option(line)}</p>)} */}
-              </div>
-            ))}
+        <div className="container d-flex justify-content-center row">
+          <div className="answer-container m-1 col-3">
+            {piece.sequence.map(partName => this.renderPart(partName))}
           </div>
-          <div className="answer-container">
-            {piece.parts.map(part => (
-              <div>
-                <p>
-                  <strong>{part.name}</strong>
-                </p>
-                {part.lines.map(line => <p>{line}</p>)}
-                {/* {part.lines.map(line => <p>{option(line)}</p>)} */}
-              </div>
-            ))}
+          <div className="form-container m-1 col-3">
+            {piece.sequence.map(partName => this.renderPart(partName, true))}
+          </div>
+          <div className="result-container m-1 col-2">
+            {piece.sequence.map(partName => this.renderPart(partName))}
           </div>
         </div>
       </div>
@@ -171,7 +74,6 @@ class Test extends Component {
 
   render() {
     return this.renderPiece()
-    // return (<div>Works</div>)
   }
 }
 
