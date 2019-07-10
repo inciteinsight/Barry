0f9576d0feaee0baefe6af06c5f552136892d695
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-09 23:45:22 -0400

    Initializing Redux without using Backend

--- client/store/piece.js
+++ client/store/piece.js
@@ -1,4 +1,9 @@
-import axios from 'axios'
+import {MidYear2019, HintOptions} from './lyricTempStore'
+
+const initialState = {
+  selectedPiece: MidYear2019[0],
+  selectedHint: HintOptions[0]
+}
 
 const GET_PIECE = 'GET_PIECE'
 
@@ -7,11 +12,23 @@ const getPiece = selectedPiece => ({
   selectedPiece
 })
 
-// export const getPieceThunk = id => async dispatch => {
-//     try {
-//         const { data } =
-//     }
-//     catch (error) {
-//         console.error(error)
-//     }
-// }
+export const getPieceThunk = pieceId => async dispatch => {
+  try {
+    const data = await MidYear2019.find(piece => piece.id === pieceId)
+    console.log(data)
+    dispatch(getPiece(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default function(state = initialState, action) {
+  let newState = {...state}
+  switch (action.type) {
+    case GET_PIECE:
+      newState.selectedPiece = action.selectedPiece
+      return newState
+    default:
+      return state
+  }
+}
