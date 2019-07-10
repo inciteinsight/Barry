1828d6b02468770a03a47b9df61e7e7c74787335
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-10 14:25:53 -0400

    Added Redux Hint

--- client/store/piece.js
+++ client/store/piece.js
@@ -6,10 +6,16 @@ const initialState = {
 }
 
 const GET_PIECE = 'GET_PIECE'
+const GET_HINT = 'GET_HINT'
 
 const getPiece = selectedPiece => ({
   type: GET_PIECE,
   selectedPiece
+})
+
+const getHint = selectedHint => ({
+  type: GET_HINT,
+  selectedHint
 })
 
 export const getPieceThunk = pieceId => async dispatch => {
@@ -22,11 +28,24 @@ export const getPieceThunk = pieceId => async dispatch => {
   }
 }
 
+export const getHintThunk = hintName => async dispatch => {
+  try {
+    const data = await HintOptions.find(opt => opt.name === hintName)
+    console.log(data)
+    dispatch(getHint(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export default function(state = initialState, action) {
   let newState = {...state}
   switch (action.type) {
     case GET_PIECE:
       newState.selectedPiece = action.selectedPiece
+      return newState
+    case GET_HINT:
+      newState.selectedHint = action.selectedHint
       return newState
     default:
       return state
