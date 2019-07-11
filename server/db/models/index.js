ba665bdd67e888e208d90930a4981a9633cacb53
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-11 08:17:38 -0400

    Fixed Seuqlize Errors

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,23 +3,11 @@ const Line = require('./line')
 const Part = require('./part')
 const Piece = require('./piece')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-// Line.belongsTo(Part);
-// Part.hasMany(Line);
-// Part.belongsTo(Piece);
-// Piece.hasMany(Part);
+Line.belongsTo(Part)
+Part.hasMany(Line)
+Part.belongsTo(Piece)
+Piece.hasMany(Part)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
   User,
   Line,
