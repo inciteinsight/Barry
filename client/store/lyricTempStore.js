2224ff4a59f92d359a3646d1081f805f6f8f9982
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-21 11:24:50 -0400

    Currently Adding Stars by Les Miserables to Generic

--- client/store/lyricTempStore.js
+++ client/store/lyricTempStore.js
@@ -333,3 +333,297 @@ export const HintOptions = [
       line.replace(/\w[0-9A-Za-z_',!]*/g, sel => sel[0]).toUpperCase()
   }
 ]
+
+export const GenericSongsSpeeches = [
+  {
+    id: 1,
+    name: 'Stars, from Les Miserables',
+    access: null,
+    sequence: ['chorus1', 'stanza1', 'chorus2', 'stanza2', 'bridge', 'finale'],
+    parts: [
+      {
+        name: 'chorus1',
+        lines: [
+          'There, out in the darkness',
+          'A fugitive running',
+          'Fallen from God',
+          'Fallen from grace',
+          'God be my witness',
+          'I never shall yield',
+          'Til we come face to face',
+          'Til we come face to face'
+        ]
+      },
+      {
+        name: 'stanza1',
+        lines: [
+          'Great indeed is Your unfailing love',
+          'For us, O Lord God',
+          'You have guided us and we remain',
+          'Your true chosen ones',
+          'And this is a special day',
+          'In your blessed and dear Church of Christ'
+        ]
+      },
+      {
+        name: 'refrain1',
+        lines: [
+          'He knows his way in the dark',
+          'Mine is the way of the Lord',
+          'Those who follow the path of the righteous',
+          'Shall have their reward',
+          'And if they fall as Lucifer fell',
+          'The flames',
+          'The sword!'
+        ]
+      },
+      {
+        name: 'stanza3',
+        lines: [
+          'Through fierce persecution, You are there',
+          'Defending Your fold',
+          'In the darkest hours of our distress',
+          'You rescue Your own',
+          'O Lord God, You are so good',
+          'To your Church, where Your grace is bestowed'
+        ]
+      },
+      {
+        name: 'stanza4',
+        lines: [
+          'Your last mission of salvation grows',
+          'Now spanning the globe',
+          'Many join to praise and worship you',
+          'In reverence Lord',
+          'The true chosen who will reap',
+          'The reward that You have laid instore'
+        ]
+      },
+      {
+        name: 'stanza5',
+        lines: [
+          "All our efforts, we'll intensify",
+          'To help spread the truth',
+          'In the work of propagation, Lord',
+          'To always bear fruit',
+          'So that you are greatly pleased',
+          'We will spend all our days serving You'
+        ]
+      },
+      {
+        name: 'stanza6',
+        lines: [
+          'Lord, to Your whole nation, You give heed',
+          'And answer our cries',
+          'That the church administration, Lord',
+          "You'll care for and guide",
+          'So that we will all be led',
+          'To achieve perfect faith in Your sight'
+        ]
+      },
+      {
+        name: 'coda',
+        lines: [
+          'We will not separate from the right',
+          'To worship You, which You bestowed on us',
+          'Amen'
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Mid-Year 2019 CWS Processional',
+    access: 'cni',
+    sequence: [
+      'intro',
+      'stanza1',
+      'refrain',
+      'stanza3',
+      'stanza4',
+      'refrain',
+      'stanza5',
+      'refrain',
+      'coda'
+    ],
+    parts: [
+      {
+        name: 'intro',
+        lines: [
+          'Father dear, to You all praise and honor',
+          'Oh, Your Name we worship and adore!',
+          'Your divine love is beyond all measure',
+          'For the nation You have chosen, Lord'
+        ]
+      },
+      {
+        name: 'stanza1',
+        lines: [
+          'We now offer our sincere thanksgiving',
+          'For Your goodness which has filled our lives',
+          'And the victories and countless blessings',
+          'That You grant the one true Church of Christ'
+        ]
+      },
+      {
+        name: 'stanza2',
+        lines: [
+          'Through all trials and all persecutions',
+          'Through the many hardships that have come',
+          'You are always there to help Your children',
+          'Oh You are, indeed, so good to us!'
+        ]
+      },
+      {
+        name: 'refrain',
+        lines: [
+          'With the highest regard, we will uphold',
+          'This divine calling You bestowed, O Lord',
+          'We will never waste our great election',
+          'We will love this blessing all the more'
+        ]
+      },
+      {
+        name: 'stanza3',
+        lines: [
+          'Now Your Church is growing and progressing',
+          'Reaching nations everywhere on Earth',
+          'For Your praise, the members are increasing',
+          'Joining in true worship in Your Church'
+        ]
+      },
+      {
+        name: 'stanza4',
+        lines: [
+          'While we still have life and strength, O Father',
+          'We will help in missionary work',
+          'And we promise, we will always serve You',
+          'So that we will always please You, Lord'
+        ]
+      },
+      {
+        name: 'stanza5',
+        lines: [
+          'Thank You for remaining with and guiding',
+          'The administration that You placed',
+          'May you always care for and protect them',
+          'So that we are led to perfect faith'
+        ]
+      },
+      {
+        name: 'coda',
+        lines: [
+          'Father, help us, oh, help us all our days',
+          'To be faithful and never leave Your Side',
+          'So that we will all receive salvation',
+          'And the promised everlasting life',
+          'Amen'
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Mid-Year 2019 AWS Spanish Processional',
+    access: 'cni',
+    sequence: [
+      'stanza1',
+      'stanza2',
+      'estribillo',
+      'stanza3',
+      'stanza4',
+      'estribillo',
+      'stanza5',
+      'stanza6',
+      'estribillo',
+      'coda'
+    ],
+    parts: [
+      {
+        name: 'stanza1',
+        lines: [
+          'Nuestro Dios, grandioso es Tu amor',
+          'Por Tu santa nacion',
+          'Y a nosotros, Tus elegidos',
+          'Nos guias te sin cesar',
+          'Hoy es el dia especial',
+          'De Tu amada Iglesia De Cristo'
+        ]
+      },
+      {
+        name: 'stanza2',
+        lines: [
+          'Ahora estamos celebrandolo',
+          'Te da mos las gracias',
+          'Por las bendiciones y exitos',
+          'Que provienen de Ti',
+          'Toda prueba y dificultad',
+          'Por Tu ayuda pudimos superar'
+        ]
+      },
+      {
+        name: 'estribillo',
+        lines: [
+          'Oh, jamas desperdiciaremos',
+          'La preciada y grandiosa eleccion',
+          'A causa de la redencion',
+          'Cuando Jesucristo nos redimio',
+          'Oh, jamas nos vamos a separar',
+          'Del derecho que nos diste, nuestro Dios'
+        ]
+      },
+      {
+        name: 'stanza3',
+        lines: [
+          'De la severa persecusion',
+          'Nos Has defendido',
+          'Y de la intensa dificultad',
+          'Nos has rescatado',
+          'Bondadoso eres, nuestro Dios',
+          'A tu Iglesia concedes bendicion'
+        ]
+      },
+      {
+        name: 'stanza4',
+        lines: [
+          'Tu santa obra de salvacion',
+          'Ya esta extendida',
+          'Y los que Te adoran de verdad',
+          'No dejan de aumentar',
+          'Solo Tus elegidos',
+          'Obtendran el preciado galardon'
+        ]
+      },
+      {
+        name: 'stanza5',
+        lines: [
+          'La obra de propagacion',
+          'Vamos a intensificar',
+          'Ayudando todos mucho mas,',
+          'Frutos vamos a dar',
+          'Para que se as agradado',
+          'Siempre Te serviremos, Senor'
+        ]
+      },
+      {
+        name: 'stanza6',
+        lines: [
+          'Padre, Tu escuchas la oracion',
+          'De tu santa nacion',
+          'De cuidar a la Administracion',
+          'De Tu Iglesia',
+          'Para que nos pueda presentar',
+          'Perfectos ante Ti, nuestro amado Dios'
+        ]
+      },
+      {
+        name: 'coda',
+        lines: [
+          'Oh, jamas nos vamos a separar',
+          'Del derecho que nos diste, nuestro Dios',
+          'Amen'
+        ]
+      }
+    ]
+  }
+]
